c078d883a7a95d4e8d2feb66a68fa34a632415bf
User <user@user.com>
2021-06-11 23:02:30 +0200

    Radim jako da zavrsim sto pre

--- PgpClient/src/main/java/etf/openpgp/iu170057d_sm170081d/encryption/Encryption.java
+++ PgpClient/src/main/java/etf/openpgp/iu170057d_sm170081d/encryption/Encryption.java
@@ -454,32 +454,129 @@ public class Encryption
         return message;
     }
     
+    public static class PgpDecryptionState
+    {
+        PGPEncryptedDataList encryptedDataList = null;
+        Object pgpObject = null;
+        Object currentMessage = null;
+        PGPObjectFactory pgpObjectFactory = null;
+        PGPPublicKeyEncryptedData publicKeyEncryptedData = null;
+        PGPOnePassSignature onePassSignature = null;
+        PGPPublicKey signerPublicKey = null;
+    }
+    
     public static InputStream removeRadix64Encoding(InputStream inputStream) throws IOException
     {
         return PGPUtil.getDecoderStream(new BufferedInputStream(inputStream));
     }
     
-    public static void decrypt(PgpMessage pgpMessage)
+    public static void decrypt(
+            PgpMessage pgpMessage,
+            PgpDecryptionState pds,
+            char[] passphrase) throws PGPException, IOException
+    {
+        if (!pgpMessage.isEncrypted)
+        {
+            return;
+        }
+        
+        PGPPrivateKey secretKey = null;
+        
+        Iterator<PGPEncryptedData> it = pds.encryptedDataList.getEncryptedDataObjects();
+
+        PGPSecretKeyRingCollection pgpSecretKeyRingCollection = PGPKeys.getSecretKeysCollection();
+        while (secretKey == null && it.hasNext())
+        {
+            pds.publicKeyEncryptedData = (PGPPublicKeyEncryptedData) it.next();
+            PGPSecretKey pgpSecKey = pgpSecretKeyRingCollection.getSecretKey(pds.publicKeyEncryptedData.getKeyID());
+
+            if (pgpSecKey != null)
+            {
+                Provider provider = Security.getProvider("BC");  
+                secretKey = pgpSecKey.extractPrivateKey(
+                        new JcePBESecretKeyDecryptorBuilder(
+                                new JcaPGPDigestCalculatorProviderBuilder()
+                                        .setProvider(provider)
+                                        .build())
+                                .setProvider(provider)
+                                .build(passphrase));
+            }
+        }
+
+        // Secret key not found in private key ring collection - not possible to decrypt
+        if (secretKey == null)
+        {
+            throw new IllegalArgumentException("Secret key for message not found.");
+        }
+        // Secret key found and message is decrypted
+        else
+        {
+            System.out.println("Decryption successful!");
+        }
+
+        InputStream clear = pds.publicKeyEncryptedData.getDataStream(
+                new JcePublicKeyDataDecryptorFactoryBuilder()
+                        .setProvider("BC")
+                        .build(secretKey)); 
+        pds.pgpObjectFactory = new PGPObjectFactory(clear, null);
+        pds.currentMessage = pds.pgpObjectFactory.nextObject();
+    }
+    
+    public static void decompress(
+            PgpMessage pgpMessage,
+            PgpDecryptionState pds) throws IOException, PGPException
+    {
+        pgpMessage.isCompressed = false;
+        if (pds.currentMessage instanceof PGPCompressedData)
+        {
+            pgpMessage.isCompressed = true;
+            PGPCompressedData compressedData = (PGPCompressedData) pds.currentMessage;
+            pds.pgpObjectFactory = new PGPObjectFactory(new BufferedInputStream(compressedData.getDataStream()), null);
+            pds.currentMessage = pds.pgpObjectFactory.nextObject();
+        }
+    }
+    
+    public static void checkIfSigned(PgpMessage pgpMessage, PgpDecryptionState pds) throws PGPException, IOException
+    {
+        // Determined if the message is signed
+        pgpMessage.isSigned = false;
+        if (pds.currentMessage instanceof PGPOnePassSignatureList)
+        {
+            PGPOnePassSignatureList p1 = (PGPOnePassSignatureList) pds.currentMessage;
+            pds.onePassSignature = p1.get(0);
+            long keyId = pds.onePassSignature.getKeyID();
+            pgpMessage.isSigned = true;
+            
+            // Get signer public key
+            pds.signerPublicKey = PGPKeys.getPublicKeysCollection().getPublicKey(keyId);
+
+            pds.onePassSignature.init(new JcaPGPContentVerifierBuilderProvider().setProvider("BC"), pds.signerPublicKey);
+
+            pds.currentMessage = pds.pgpObjectFactory.nextObject();
+        }
+    }
+    
+    public static void unpackLiteral(
+            PgpMessage pgpMessage,
+            PgpDecryptionState pds) throws PGPException, IOException
     {
     }
     
-    public static void decompress(PgpMessage pgpMessage)
+    public static void verifyIntegrity(
+            PgpMessage pgpMessage,
+            PgpDecryptionState pds) throws PGPException, IOException
     {
     }
     
-    public static void unpackLiteral(PgpMessage pgpMessage)
+    public static void verifySignature(
+            PgpMessage pgpMessage,
+            PgpDecryptionState pds) throws PGPException, IOException
     {
     }
     
-    public static void verifyIntegrity(PgpMessage pgpMessage)
-    {
-    }
-    
-    public static void verifySignature(PgpMessage pgpMessage)
-    {
-    }
-    
-    public static void getSignature(PgpMessage pgpMessage)
+    public static void getSignature(
+            PgpMessage pgpMessage,
+            PgpDecryptionState pds) throws PGPException, IOException
     {
     }
     
@@ -509,12 +606,6 @@ public class Encryption
         }
     }
     
-    public static class PgpDecryptionState
-    {
-        PGPEncryptedDataList encryptedDataList = null;
-        Object pgpObject = null;
-    }
-    
     public static void readPgpMessage(PgpMessage pgpMessage) throws Exception
     {
         InputStream inputStream = new ByteArrayInputStream(pgpMessage.encryptedMessage);
@@ -534,110 +625,43 @@ public class Encryption
     
     public static void decryptPgpMessage(
             char[] passphrase,
-            PgpMessage pgpMessage) throws Exception
+            PgpMessage pgpMessage) throws IOException, PGPException
     {
-        Object message = null;
-        PGPObjectFactory pgpObjectFactory = null;
+        PgpDecryptionState pds = new PgpDecryptionState();
         
         InputStream inputStream = new ByteArrayInputStream(pgpMessage.encryptedMessage);
         inputStream = removeRadix64Encoding(inputStream);
         
-        PgpDecryptionState pds = new PgpDecryptionState();
         checkIfEncrypted(inputStream, pgpMessage, pds);
-        System.out.println("encryptedDataList: " + pds.encryptedDataList);
-        System.out.println("pgpObject: " + pds.pgpObject);
 
-        // If the message is encrypted, try to decrypt it
-        PGPPrivateKey secretKey = null;
-        PGPPublicKeyEncryptedData publicKeyEncryptedData = null;
-        if (pgpMessage.isEncrypted) 
+        if (pgpMessage.isEncrypted)  // Message is encrypted, try to decrypt it
         {
-            Iterator<PGPEncryptedData> it = pds.encryptedDataList.getEncryptedDataObjects();
-
-            PGPSecretKeyRingCollection pgpSecretKeyRingCollection = PGPKeys.getSecretKeysCollection();
-            while (secretKey == null && it.hasNext())
-            {
-                publicKeyEncryptedData = (PGPPublicKeyEncryptedData) it.next();
-                PGPSecretKey pgpSecKey = pgpSecretKeyRingCollection.getSecretKey(publicKeyEncryptedData.getKeyID());
-
-                if (pgpSecKey != null)
-                {
-                    Provider provider = Security.getProvider("BC");  
-                    secretKey = pgpSecKey.extractPrivateKey(
-                            new JcePBESecretKeyDecryptorBuilder(
-                                    new JcaPGPDigestCalculatorProviderBuilder()
-                                            .setProvider(provider)
-                                            .build())
-                                    .setProvider(provider)
-                                    .build(passphrase));
-                }
-            }
-            
-            // Secret key not found in private key ring collection - not possible to decrypt
-            if (secretKey == null)
-            {
-                throw new IllegalArgumentException("Secret key for message not found.");
-            }
-            // Secret key found and message is decrypted
-            else
-            {
-                System.out.println("Decryption successful!");
-            }
-            
-            InputStream clear = publicKeyEncryptedData.getDataStream(
-                    new JcePublicKeyDataDecryptorFactoryBuilder()
-                            .setProvider("BC")
-                            .build(secretKey)); 
-            pgpObjectFactory = new PGPObjectFactory(clear, null);
-            message = pgpObjectFactory.nextObject();
+            decrypt(pgpMessage, pds, passphrase);
         }
-        // Message is not encrypted
-        else
+        else  // Message is not encrypted
         {
-            message = pds.pgpObject;
+            pds.currentMessage = pds.pgpObject;
         }
         
-        // Decompress
-        if (message instanceof PGPCompressedData)
-        {
-            pgpMessage.isCompressed = true;
-            PGPCompressedData compressedData = (PGPCompressedData) message;
-            pgpObjectFactory = new PGPObjectFactory(new BufferedInputStream(compressedData.getDataStream()), null);
-            message = pgpObjectFactory.nextObject();
-        }
-
-        // Determined if the message is signed
-        pgpMessage.isSigned = false;
-        PGPOnePassSignature ops = null;
-        PGPPublicKey signerPublicKey = null;
-        if (message instanceof PGPOnePassSignatureList)
-        {
-            PGPOnePassSignatureList p1 = (PGPOnePassSignatureList) message;
-            ops = p1.get(0);
-            long keyId = ops.getKeyID();
-            pgpMessage.isSigned = true;
-            
-            // Get signer public key
-            signerPublicKey = PGPKeys.getPublicKeysCollection().getPublicKey(keyId);
-
-            ops.init(new JcaPGPContentVerifierBuilderProvider().setProvider("BC"), signerPublicKey);
-
-            message = pgpObjectFactory.nextObject();
-            System.out.println("message2: " + message);
-        }
+        // If compressed, decompress
+        decompress(pgpMessage, pds);
         
-        if (message instanceof PGPLiteralData)
+        checkIfSigned(pgpMessage, pds);
+        
+        
+        
+        if (pds.currentMessage instanceof PGPLiteralData)
         {
-            PGPLiteralData ld = (PGPLiteralData) message;
+            PGPLiteralData ld = (PGPLiteralData) pds.currentMessage;
 
             InputStream is = ld.getInputStream();
             pgpMessage.decryptedMessage = IOUtils.toByteArray(is);
             
-            if(publicKeyEncryptedData != null)
+            if(pds.publicKeyEncryptedData != null)
             {
-                if (publicKeyEncryptedData.isIntegrityProtected())
+                if (pds.publicKeyEncryptedData.isIntegrityProtected())
                 {
-                    if (!publicKeyEncryptedData.verify())
+                    if (!pds.publicKeyEncryptedData.verify())
                     {
                         throw new PGPException("message failed integrity check");
                     }
@@ -651,11 +675,11 @@ public class Encryption
             // Read signature
             if (pgpMessage.isSigned)
             {
-                ops.update(pgpMessage.decryptedMessage);
-                PGPSignatureList p3 = (PGPSignatureList) pgpObjectFactory.nextObject();
-                if (ops.verify(p3.get(0)))
+                pds.onePassSignature.update(pgpMessage.decryptedMessage);
+                PGPSignatureList p3 = (PGPSignatureList) pds.pgpObjectFactory.nextObject();
+                if (pds.onePassSignature.verify(p3.get(0)))
                 {
-                    String str = new String((byte[]) signerPublicKey.getRawUserIDs().next(),StandardCharsets.UTF_8);
+                    String str = new String((byte[]) pds.signerPublicKey.getRawUserIDs().next(),StandardCharsets.UTF_8);
                     System.out.println("Signature verified: " + str);
                 }
                 else
